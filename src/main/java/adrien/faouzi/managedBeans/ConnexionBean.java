00278399d247c214bdf743dfc4dbd18b6d44aabc
adrien <adrien.vandeverre@promsocatc.net>
2022-12-29 12:00:45 +0100

    vérification back-end 29-12-22

--- src/main/java/adrien/faouzi/managedBeans/ConnexionBean.java
+++ src/main/java/adrien/faouzi/managedBeans/ConnexionBean.java
@@ -2,13 +2,19 @@ package adrien.faouzi.managedBeans;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import java.io.Serializable;
 
 @Named
 @RequestScoped
 public class ConnexionBean implements Serializable {
 
-    private String name;
+    /**
+     * field
+     */
+    private String mail;
+
     private String password;
 
     public String goToPageConnexion()
@@ -18,6 +24,27 @@ public class ConnexionBean implements Serializable {
 
     public String goToPageAccueil(){
         return "accueil";
+    }
+
+    /**
+     * getter and setter method
+     * @return
+     */
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
 
